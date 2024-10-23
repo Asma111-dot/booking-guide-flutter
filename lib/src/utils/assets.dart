0f85770c6f0e1fdbc8765218fbdf3bf83fea6899
unknown <a77s54m21a1110@gmail.com>
2024-10-23 09:18:01 +0300

    Creating files in general using the Riverpod library(2)

---
+++ lib/src/utils/assets.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+
+const _base = 'assets/';
+const _lottieBase = "${_base}lottie/";
+const _imageBase = "${_base}images/";
+const _iconBase = "${_base}icons/";
+
+// Lottie (Json) ---------------------------------------------------------------
+const loadingJson = "${_lottieBase}loading_circular.json";
+const warningJson = "${_lottieBase}warning.json";
+const successJson = "${_lottieBase}success.json";
+const errorJson = "${_lottieBase}error.json";
+const infoJson = "${_lottieBase}info.json";
+const emptyJson = "${_lottieBase}empty.json";
+const welcomeJson = "${_lottieBase}welcome.json";
+
+// Images ----------------------------------------------------------------------
+const logoCoverImage = "${_imageBase}logo_cover.png";
+
+// Icons -----------------------------------------------------------------------
+const externalLinkIcon = Feather.external_link;
+const homeIcon = Feather.home;
+const userIcon = Feather.user;
+const settingsIcon = Feather.settings;
+const languageIcon = Feather.globe;
+const darkModeIcon = Feather.moon;
+const privacyPolicyIcon = Feather.lock;
+const termsOfUseIcon = Feather.shield;
+const searchIcon = Feather.search;
+const imageIcon = Feather.image;
+const dropDownIcon = Icons.arrow_drop_down_rounded;
+const infoIcon = Feather.info;
+const dateIcon = Feather.calendar;
+const radioOnIcon = Ionicons.radio_button_on_outline;
+const radioOffIcon = Ionicons.radio_button_off_outline;
+const visibleIcon = Icons.visibility;
+const invisibleIcon = Icons.visibility_off;
+const addIcon = Icons.add_rounded;
+const timeIcon = Feather.clock;
+const calendarIcon = Feather.calendar;
+const starIcon = Icons.star_rounded;
+const starBorderIcon = Icons.star_border_outlined;
+const deleteIcon = Icons.delete_outline_rounded;
+const callIcon = Icons.phone_rounded;
+const phoneIcon = Icons.phone_iphone_rounded;
+const mapIcon = Icons.map;
---
+++ lib/src/utils/urls.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+String baseUrl = kDebugMode
+    ? (Platform.isIOS ? "http://localhost:8000/" : "http://10.0.2.2:8000/")
+    : "http://bookings-guide.com/";
+
+String apiUrl = "${baseUrl}api/";
+
+String apiPanelUrl(String subDomain) => "${apiUrl}app/$subDomain";
+
+String loginUrl() => "${apiUrl}login";
+String logoutUrl() => "${apiUrl}logout";
+
+String getUserUrl(int userId) => "${apiUrl}users/$userId";
+String updateUserUrl(int userId) => "${apiUrl}users/$userId";
+String deleteUserUrl(int userId) => "${apiUrl}users/$userId";
+
+String getCustomerUrl(String subDomain, int customerId) =>
+    "${apiPanelUrl(subDomain)}customers/$customerId";
+String addCustomerUrl(String subDomain) =>
+    "${apiPanelUrl(subDomain)}customers";
+String updateCustomerUrl(String subDomain, int customerId) =>
+    "${apiPanelUrl(subDomain)}customers/$customerId";
+
+String getCustomersUrl(String subDomain) =>
+    "${apiPanelUrl(subDomain)}customers";
+
